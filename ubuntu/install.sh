8985f6b34c7a9bf7b7738d491b45c8fd290c8044
Roger Souza <roger.souza@owinteractive.com>
2022-01-04 13:26:21 -0300

    WIP: Adicionado um force para limpar os dados

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -8,7 +8,7 @@ printf '\e[1;31m Entrando como usuário root \e[0m\n'
 sudo su
 
 printf '\e[1;31m Fazendo update do S.O \e[0m\n'
-sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get update -q -y --force-yes && sudo apt-get upgrade -q -y --force-yes && sudo apt-get dist-upgrade -q -y --force-yes
 
 # Loggout admin
 printf '\e[1;31m Saindo de usuário root \e[0m\n'
