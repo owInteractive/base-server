45d01f5425bf99ba4bc19e1d57ecfe95c664d033
Roger Souza <roger.souza@owinteractive.com>
2022-01-04 13:18:56 -0300

    WIP: Adicionado comandos de update do S.O

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -4,9 +4,12 @@
 # Works in ubuntu 21.10
 
 # Logging in admin
-printf '\e[1;31m Sudo su \e[0m\n'
+printf '\e[1;31m Entrando como usuário root \e[0m\n'
 sudo su
 
+printf '\e[1;31m Fazendo update do S.O \e[0m\n'
+sudo apt-get update -y && sudo apt-get upgrade -y
+
 # Loggout admin
-printf '\e[1;31m Sudo su \e[0m\n'
+printf '\e[1;31m Saindo de usuário root \e[0m\n'
 exit
