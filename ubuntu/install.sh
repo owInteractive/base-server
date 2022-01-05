c5b2d8840004e3e53e4a394e4b16b7e851e81084
Roger Souza <roger.souza@owinteractive.com>
2022-01-05 08:25:46 -0300

    FIX: Created checking of ssh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -10,6 +10,10 @@ sudo su
 printf '\e[1;31m Fazendo update do S.O \e[0m\n'
 sudo apt-get update -q -y --force-yes && sudo apt-get upgrade -q -y --force-yes && sudo apt-get dist-upgrade -q -y --force-yes
 
+printf '\e[1;31m Buscando o arquivo id_rsa.pub \e[0m\n'
+[ -f ~/.ssh/id_rsa.pub ] && echo "Encontrado" || echo "Não encontrado"
+[ -f ~/.ssh/id_rsa.pub ] && echo "Encontrado" || ssh-keygen -b 2048 -t rsa -f ~/.ssh -q -N ""
+
 # Loggout admin
 printf '\e[1;31m Saindo de usuário root \e[0m\n'
 exit
