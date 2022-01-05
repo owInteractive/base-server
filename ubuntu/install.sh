2e2d19bc132b43bf1e0af010d6c5053137e94152
Roger Souza <roger.souza@owinteractive.com>
2022-01-05 17:12:44 -0300

    FIX: Upadte to server

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,11 +1,7 @@
-#!/usr/bin/env sh
+#/bin/bash
 
 ##
 # Works in ubuntu 21.10
-
-# Logging in admin
-printf '\e[1;31m Entrando como usuário root \e[0m\n'
-sudo su
 
 printf '\e[1;31m Fazendo update do S.O \e[0m\n'
 sudo apt-get update -q -y --force-yes && sudo apt-get upgrade -q -y --force-yes && sudo apt-get dist-upgrade -q -y --force-yes
@@ -14,26 +10,14 @@ printf '\e[1;31m Buscando o arquivo id_rsa.pub \e[0m\n'
 [ -f ~/.ssh/id_rsa.pub ] && echo '\e[1;31m Encontrado \e[0m\n' || echo '\e[1;31m Não encontrado \e[0m\n'
 [ -f ~/.ssh/id_rsa.pub ] && echo '\e[1;31m ... \e[0m\n' || ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
-printf '\e[1;31m Copiei e cole essa chave no repositorio remoto \e[0m\n'
+printf '\e[1;31m Copiei e cole essa chave no repositorio remoto nas configurações do bitbucket \e[0m\n'
 cat ~/.ssh/id_rsa.pub
 
-# while true; do
-#     read -p '\e[1;31m Copiado? \e[0m\n' yn
-#     case $yn in
-#         [Yy]* ) break;;
-#         [Nn]* ) '\e[1;31m Por favor copie para continuar \e[0m\n';;
-#         * ) echo '\e[1;31m Responda com uma alternativa válida \e[0m\n';;
-#     esac
-# done
+read -p "Nome do projeto no bitbucket " project
+echo "$project"
 
-echo '\e[1;31m Copiado? \e[0m\n'
-select yn in "Sim" "Não"; do
-    case $yn in
-        Yes ) break;;
-        No ) exit;;
-    esac
-done
+rm -rf /var/www/html/$project
+git clone git@bitbucket.org:owinteractive/$project.git /var/www/html/$project
 
-# Loggout admin
-printf '\e[1;31m Saindo de usuário root \e[0m\n'
-exit
+
+# git clone git@bitbucket.org:owinteractive/api-nest.base.com.git
